a367f143a74f5532f961d8801a3c6883f174cbb1
Светайло Данил ИН-01 <danil.svetaylo@gmail.com>
2025-06-03 08:29:44 +0300

    Create web-service

---
+++ CurrencyConverter/Controllers/CurrencyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+        private const decimal UsdToUahRate = 39.5m;
+
+        [HttpGet("toUsd")]
+        public IActionResult ConvertToUsd([FromQuery] decimal uah)
+        {
+            var usd = uah / UsdToUahRate;
+            return Ok(usd);
+        }
+
+        [HttpGet("toUah")]
+        public IActionResult ConvertToUah([FromQuery] decimal usd)
+        {
+            var uah = usd * UsdToUahRate;
+            return Ok(uah);
+        }
+    }
+}
